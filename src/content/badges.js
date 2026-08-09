4890f932dda5810f6694f6c645378c4d1c22c1d8
barimale <mateusz.wolnica@gmail.com>
2026-08-09 21:13:55 +0200

    Update badges.js

--- src/content/badges.js
+++ src/content/badges.js
@@ -3,7 +3,7 @@ export const css = "https://img.shields.io/badge/-CSS-1572B6?logo=css3&logoColor
 export const js = "https://img.shields.io/badge/-JavaScript-F7DF1E?logo=javascript&logoColor=black&style=for-the-badge";
 export const react = "https://img.shields.io/badge/-React%20JS-61DAFB?logo=react&logoColor=black&style=for-the-badge";
 // export const mongoDb = "https://img.shields.io/badge/-Mongo%20DB-47A248?logo=mongodb&logoColor=white&style=for-the-badge";
-export const mySql = "https://img.shields.io/badge/-my%20sql-4479A1?logo=mysql&logoColor=white&style=for-the-badge";
+//export const mySql = "https://img.shields.io/badge/-my%20sql-4479A1?logo=mysql&logoColor=white&style=for-the-badge";
 export const mSSql = "https://img.shields.io/badge/-ms%20sql-4479A1?logo=mssql&logoColor=white&style=for-the-badge";
 export const tailwindCss = "https://img.shields.io/badge/-Tailwind%20css-06B6D4?logo=tailwindcss&logoColor=white&style=for-the-badge";
 export const typescript = "https://img.shields.io/badge/-typescript-3178C6?logo=typescript&logoColor=white&style=for-the-badge";
@@ -12,5 +12,5 @@ export const winforms = "https://img.shields.io/badge/-winforms-4479A1?logo=winf
 export const dotnet = "https://img.shields.io/badge/-.NET-4479A1?logo=.NET&logoColor=white&style=for-the-badge";
 
 export const badges = [
-    html, css, js, react, mySql, tailwindCss, typescript, dotnet, winforms, wpf, mSSql
+    html, css, js, react, tailwindCss, typescript, dotnet, winforms, wpf, mSSql
 ]
